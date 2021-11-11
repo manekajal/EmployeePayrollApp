2eaef75f9ae78b1e3a2996415800fd94808adbb2
Kajal <manekajal2014@gmail.com>
2021-11-11 11:10:01 +0530

    [kajal]:Reset Employee Payroll Form

--- js/EmployeePayrollForm.js
+++ js/EmployeePayrollForm.js
@@ -1,65 +1,27 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    salaryOutput();
-    validateName();
-    validateDate();
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if (name.value.length == 0) {
+            textError.textContent = " ";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = " ";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
 
-});
-
-function salaryOutput() {
-    const salary = document.querySelector('#salary');
+    const salary = document.querySelector("#salary");
     const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
     salary.addEventListener('input', function() {
         output.textContent = salary.value;
-
     });
-}
 
-function validateName() {
-    let name = document.querySelector('#name');
-    let textError = document.querySelector('.text-error');
-    name.addEventListener('input', function() {
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-        if (nameRegex.test(name.value)) {
-            textError.textContent = "";
-        } else {
-            textError.textContent = "Name is Incorrect"
-        }
-    });
-}
-
-function validateDate() {
-    let day = document.querySelector('#day');
-    let month = document.querySelector('#month');
-    let year = document.querySelector('#year');
-    day.addEventListener('input', checkDate);
-    month.addEventListener('input', checkDate);
-    year.addEventListener('input', checkDate);
-}
-
-function checkDate() {
-    let dateError = document.querySelector('.dates-error');
-    let date = day.value + " " + month.value + " " + year.value;
-    try {
-        checkStartDate(new Date(Date.parse(date)));
-        dateError.textContent = "";
-    } catch (e) {
-        dateError.textContent = e;
-    }
-
-}
-
-function checkStartDate(startDate) {
-    let currentDate = new Date();
-    if (startDate > currentDate) {
-        throw new Error("Start date is a future date")
-    }
-    let differnce = Math.abs(currentDate.getTime() - startDate.getTime());
-    let date = differnce / (1000 * 60 * 60 * 24);
-    if (date > 30) {
-        throw new Error("Start date is beyond 30 days");
-    }
-}
+});
 
 function save(event) {
     alert("save")
@@ -124,5 +86,48 @@ function createAndUpdateStorage(employeePayrollData) {
     } else {
         employeePayrollList.push(employeePayrollData);
     }
+    alert(employeePayrollList.toString());
     localStorage.setItem("EmployeePayrollListList", JSON.stringify(employeePayrollList));
 }
+
+function resetButton() {
+    setValue('#name', '');
+    setValue('#salary', '');
+    setValue('#notes', '');
+    setValue('#day', '1');
+    setValue('#month', 'January');
+    setValue('#year', '2021');
+    setTextValue('.text-error', '');
+    setTextValue('.date-error', '');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+}
+
+function setTextValue(id, value) {
+    const textError = document.querySelector(id);
+    textError.textContent = value;
+}
+
+function setValue(id, value) {
+    let element = document.querySelector(id);
+    element.value = value;
+}
+
+function unsetSelectedValues(propertyValue) {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+const createId = () => {
+    var id = localStorage.getItem("currentId");
+    if (id == undefined) {
+        localStorage.setItem("currentId", 1);
+        return 2;
+    } else {
+        id = id + 1;
+        localStorage.setItem("currentId", id);
+        return id;
+    }
+};
