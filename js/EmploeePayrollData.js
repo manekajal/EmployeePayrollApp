8fc9dc86d5734e76b17fe2538cf7a61a72bad4d1
Kajal <manekajal2014@gmail.com>
2021-11-11 10:59:11 +0530

    [kajal]:On document Load set Event Listeners

--- js/EmploeePayrollData.js
+++ js/EmploeePayrollData.js
@@ -1,5 +1,10 @@
 class EmployeePayrollData {
-    id;
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id
+    }
 
     get name() {
         return this._name;
@@ -12,7 +17,7 @@ class EmployeePayrollData {
         else
             throw new Error("Name is Incorrect");
     }
-    
+
     get profilePic() {
         return this._profilePic;
     }
@@ -52,15 +57,10 @@ class EmployeePayrollData {
         return this._startDate;
     }
 
-    set startDate(startDate) {
-        let future = new Date();
-        future.setDate(future.getDate() + 30);
-        if (startDate < new Date() || startDate < future)
-            this._startDate = startDate;
-        else
-            throw new Error("Start Date is Correct");
+    set startDate(startDate){
+        this._startDate=startDate;
     }
-
+    
     get notes() {
         return this._notes;
     }
@@ -71,53 +71,9 @@ class EmployeePayrollData {
 
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
-        return "Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
-    }
-}
-
-function save() {
-
-    let employeeName = document.querySelector('#name').value;
-    let profileList = document.querySelectorAll('input[name="profile"]');
-    let employeeProfileImage;
-    for (let image of profileList) {
-        if (image.checked) {
-            employeeProfileImage = image.value;
-            break;
-        }
-    }
-
-    let genderList = document.querySelectorAll('input[name="gender"]');
-    let employeeGender;
-    for (let gender of genderList) {
-        if (gender.checked) {
-            employeeGender = gender.value;
-            break;
-        }
-    }
-
-    let departmentList = document.querySelectorAll('.checkbox:checked');
-    let employeeDepartment = new Array();
-    for (let department of departmentList) {
-        if (department.checked) {
-            employeeDepartment.push(department.value);
-        }
-    }
-
-    let employeeSalary = document.querySelector('#salary').value;
-
-    let day = document.querySelector('#day').value;
-    let month = document.querySelector('#month').value;
-    let year = document.querySelector('#year').value;
-    let employeeStartDate = new Date(year, month, day);
-
-    let employeeNotes = document.querySelector('#notes').value;
-
-    try {
-        let employeePayrollData = new EmployeePayrollData(employeeName, employeeProfileImage, employeeGender, employeeDepartment, employeeSalary, employeeStartDate, employeeNotes);
-        console.log(employeePayrollData.toString());
-    } catch (e) {
-        console.error(e);
+        const employeeDate = this.startDate == undefined ? "undefined" : 
+        this.startDate.toLocaleDateString("en-us", options);
+        return "id=" + this.id + "Name = " + this.name + ", profilePic = " + this.profilePic + ", Gender = " + this.gender + 
+        ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
     }
 }
